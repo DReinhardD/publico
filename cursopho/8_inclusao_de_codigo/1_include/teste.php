50f6d78f6045674faaa1ef30df7b8b3448d819d4
DReinhardD <132316033+DReinhardD@users.noreply.github.com>
2023-09-13 07:07:19 -0300

    Add files via upload

---
+++ cursopho/8_inclusao_de_codigo/1_include/teste.php
@@ -0,0 +1,5 @@
+<?php
+$c =5;
+?>
+
+<h1>testando o include</h1>
---
+++ cursopho/8_inclusao_de_codigo/2_require/index.php
@@ -0,0 +1,14 @@
+<p>teste</p>
+<?php 
+
+require "teste.php";
+
+?>
+
+<p>Arquivo do include</p>
+
+<?php 
+
+require "arquivos/funcao.php";
+
+?>
